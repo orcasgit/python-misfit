c1ac57d92c73a29613d779958e9fa6647a9cf568
Brad Pitcher <bradpitcher@gmail.com>
2015-01-31 18:23:26 -0800

    use a copyright range

--- misfit/__init__.py
+++ misfit/__init__.py
@@ -16,6 +16,6 @@ __all__ = ['API_URL', 'Misfit', 'MisfitProfile', 'MisfitDevice', 'MisfitGoal',
 __title__ = 'misfit'
 __author__ = 'ORCAS'
 __author_email__ = 'bpitcher@orcasinc.com'
-__copyright__ = 'Copyright 2015 ORCAS'
+__copyright__ = 'Copyright 2014-2015 ORCAS'
 __license__ = 'Apache 2.0'
 __version__ = '0.2.0'
