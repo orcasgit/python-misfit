f0ff4ca5423b2c228b703d40bbfa8888f0550da5
Brad Pitcher <bradpitcher@gmail.com>
2015-10-02 15:10:09 -0700

    freshen up a bit
    - Support Python 3.5, stop supporting Python 3.2 (several dependencies don't support it)
    - Stop using deprecated Sphinx theme
    - Upgrade dependencies
    - Let slumber > 0.7 handle data serialization in Python > 3.2

--- misfit/misfit.py
+++ misfit/misfit.py
@@ -6,17 +6,10 @@ import sys
 from oauthlib.oauth2 import Client
 from requests_oauthlib import OAuth2
 from slumber.exceptions import HttpClientError, HttpServerError
-from slumber.serialize import Serializer, JsonSerializer
 
 from .exceptions import MisfitException, MisfitHttpException
 
 API_URL = 'https://api.misfitwearables.com/'
-
-
-class MisfitSerializer(JsonSerializer):
-    """ Override the built-in JSON serializer to handle bytes """
-    def loads(self, data):
-        return json.loads(data.decode('utf8'))
 
 
 class Misfit:
@@ -24,9 +17,8 @@ class Misfit:
         auth = OAuth2(client_id, Client(client_id),
                       {'access_token': access_token})
         user = user_id if user_id else 'me'
-        s = Serializer(default="json", serializers=[MisfitSerializer()])
         self.api = slumber.API('%smove/resource/v1/user/%s/' % (API_URL, user),
-                               auth=auth, serializer=s)
+                               auth=auth)
 
     def profile(self, object_id=None):
         return MisfitProfile(self._get_object(self.api.profile, object_id))
